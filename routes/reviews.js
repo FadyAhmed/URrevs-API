d259f26cc07998810651d526496f4d172baa757c
FadyAhmed <fady.999666@gmail.com>
2022-02-13 03:18:07 +0200

    atlas added and before heruko

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,58 +1,77 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Review = require('../models/reviews');
-const User = require('../models/user');
+const Review = require("../models/reviews");
+const User = require("../models/user");
 
 // get reviews
-router.get('/reviews', function (req, res) {
-    const limit = 5;
-    const page = parseInt(req.query.page);
-    const brand = req.query.brand;
-    const product = req.query.product;
-    const startIndex = limit * page;
-    if (brand != undefined && product != undefined) {
-        Review.find({}, { "likes": 0, "comments": 0 }).sort("-date_rev").where("brand", brand).where("product", product).limit(limit).skip(startIndex).then(function (revs) {
-            res.send(revs);
-        });
-    } else if (brand != undefined) {
-        Review.find({}, { "likes": 0, "comments": 0 }).sort("-date_rev").where("brand", brand).limit(limit).skip(startIndex).then(function (revs) {
-            res.send(revs);
-        });
-    } else {
-        Review.find({}, { "likes": 0, "comments": 0 }).sort("-date_rev").limit(limit).skip(startIndex).then(function (revs) {
-            res.send(revs);
-        });
-    }
+router.get("/reviews", function (req, res) {
+  const limit = 5;
+  const page = parseInt(req.query.page);
+  const brand = req.query.brand;
+  const product = req.query.product;
+  const startIndex = limit * page;
+  if (brand != undefined && product != undefined) {
+    Review.find({}, { likes: 0, comments: 0 })
+      .sort("-date_rev")
+      .where("brand", brand)
+      .where("product", product)
+      .limit(limit)
+      .skip(startIndex)
+      .then(function (revs) {
+        res.send(revs);
+      });
+  } else if (brand != undefined) {
+    Review.find({}, { likes: 0, comments: 0 })
+      .sort("-date_rev")
+      .where("brand", brand)
+      .limit(limit)
+      .skip(startIndex)
+      .then(function (revs) {
+        res.send(revs);
+      });
+  } else {
+    Review.find({}, { likes: 0, comments: 0 })
+      .sort("-date_rev")
+      .limit(limit)
+      .skip(startIndex)
+      .then(function (revs) {
+        res.send(revs);
+      });
+  }
 });
 
 // create review
-router.post('/review', function (req, res, next) {
-    var phone_name = req.body.brand + ' ' + req.body.product;
-    var points = (req.body.pros.length + req.body.cons.length) / 4;
+router.post("/review", function (req, res, next) {
+  var phone_name = req.body.brand + " " + req.body.product;
+  var points = (req.body.pros.length + req.body.cons.length) / 4;
 
-    User.findByIdAndUpdate({ _id: req.body.user_id }
-        , { $addToSet: { "reviews_id": phone_name }, $inc: { points: points } }
-        , function (err, doc) { });
+  User.findByIdAndUpdate(
+    { _id: req.body.user_id },
+    { $addToSet: { reviews_id: phone_name }, $inc: { points: points } },
+    function (err, doc) {}
+  );
 
-    // TODO: check if user is famous
-    Review.create(req.body).then(function (rev) {
-        res.send(rev);
-    }).catch(next);
+  // TODO: check if user is famous
+  Review.create(req.body)
+    .then(function (rev) {
+      res.send(rev);
+    })
+    .catch(next);
 });
 
 // delete review (admins)
-router.delete('/reviews/:reviewId', function (req, res, next) {
-    Review.findByIdAndRemove({ _id: req.params.reviewId }, function (err, doc) {
-        if (doc != null) {
-            if (err == null) {
-                res.send({ message: "Succesfully deleted", doc: doc });
-            } else {
-                res.send(err);
-            }
-        } else {
-            res.send({ res: "this one deleted before" });
-        }
-    })
+router.delete("/reviews/:reviewId", function (req, res, next) {
+  Review.findByIdAndRemove({ _id: req.params.reviewId }, function (err, doc) {
+    if (doc != null) {
+      if (err == null) {
+        res.send({ message: "Succesfully deleted", doc: doc });
+      } else {
+        res.send(err);
+      }
+    } else {
+      res.send({ res: "this one deleted before" });
+    }
+  });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,42 +1,54 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const cron = require('node-cron');
-const Brand = require('./models/brand');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cron = require("node-cron");
+const Brand = require("./models/brand");
+const morgan = require("morgan");
 
 // set up app
 const app = express();
 
+const uri =
+  "mongodb+srv://user:5654@urrevsdemo.bungf.mongodb.net/urrevs?retryWrites=true&w=majority";
+// const uri = "'mongodb://localhost/urrevs";
+
 // connect to mongodb
-mongoose.set('useFindAndModify', false);
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useUnifiedTopology', true);
-mongoose.connect('mongodb://localhost/urrevs', { useNewUrlParser: true });
+mongoose.set("useFindAndModify", false);
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useUnifiedTopology", true);
+
+mongoose.connect(uri, (err) => {
+  if (err) console.log(err);
+});
+
 mongoose.Promise = global.Promise;
+
+app.use(morgan("tiny"));
 
 app.use(bodyParser.json());
 
-app.use('/urrevs', require('./routes/reviews'));
-app.use('/urrevs', require('./routes/likes'));
-app.use('/urrevs', require('./routes/comments'));
-app.use('/urrevs', require('./routes/questions'));
-app.use('/urrevs', require('./routes/user'));
-app.use('/urrevs', require('./routes/article'));
-app.use('/urrevs', require('./routes/phone'));
-app.use('/urrevs', require('./routes/brand_routes'));
+app.use("/urrevs", require("./routes/reviews"));
+app.use("/urrevs", require("./routes/likes"));
+app.use("/urrevs", require("./routes/comments"));
+app.use("/urrevs", require("./routes/questions"));
+app.use("/urrevs", require("./routes/user"));
+app.use("/urrevs", require("./routes/article"));
+app.use("/urrevs", require("./routes/phone"));
+app.use("/urrevs", require("./routes/brand_routes"));
 
 // handling error
 app.use(function (error, req, res, next) {
-    res.status(422).send(error.message);
+  res.status(422).send(error.message);
 });
 
 // listen for requests
-app.listen(process.env.port || 4000, function () {
-    console.log('listing');
-    cron.schedule("0 */1 * * *", function () {
-        console.log('d');
-        Brand.updateMany(
-            {},
-            { $pull: { story: { until: { $lt: Math.floor(Date.now() / 1000) } } } });
-    });
+app.listen(4000, function () {
+  console.log("listening");
+  cron.schedule("0 */1 * * *", function () {
+    console.log("d");
+    Brand.updateMany(
+      {},
+      { $pull: { story: { until: { $lt: Math.floor(Date.now() / 1000) } } } }
+    );
+  });
 });
